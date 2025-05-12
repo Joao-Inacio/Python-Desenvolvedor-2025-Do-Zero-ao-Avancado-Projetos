4b88c4f8295b61ce0beff202887ac8bb6bdb33e6
Joao-Inacio <joaoinacio206@protonmail.com>
2025-05-12 15:11:33 -0300

    Tipos de dados

---
+++ 0.1-Fundamentos/0.02-dados.py
@@ -0,0 +1,15 @@
+# PythonFlix
+
+
+name = "Kingsman"
+yearLaunch = 2017
+noteMovie = 9.2
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(planIncluded))
