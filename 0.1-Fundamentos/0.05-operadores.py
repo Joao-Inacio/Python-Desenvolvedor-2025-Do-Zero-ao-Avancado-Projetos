8413b37bb4a478d86004ce05f6cfdffebdd350c9
Joao-Inacio <joaoinacio206@protonmail.com>
2025-05-13 16:23:17 -0300

    Operadores aritimeticos

---
+++ 0.1-Fundamentos/0.05-operadores.py
@@ -0,0 +1,17 @@
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+
+maior = num1 > num2
+menor = num1 < num2
+igual = num1 == num2
+diferente = num1 != num2
+maior_igual = num1 >= num2
+menor_igual = num1 <= num2
