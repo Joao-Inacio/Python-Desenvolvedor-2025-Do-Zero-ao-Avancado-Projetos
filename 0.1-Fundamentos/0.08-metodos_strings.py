f4fd80ae2d2ac9ec7d0569ab0cb1c941cbfbf4da
Joao-Inacio <joaoinacio206@protonmail.com>
2025-05-14 14:11:38 -0300

    Metodos de Strings

---
+++ 0.1-Fundamentos/0.08-metodos_strings.py
@@ -0,0 +1,9 @@
+nome_filmes = "Top Gun"
+
+print(nome_filmes.upper())
+print(nome_filmes.lower())
+print(nome_filmes.capitalize())
+print(nome_filmes.title())
+print(nome_filmes.center(10, '-'))
+print(nome_filmes.find("u"))
+print(nome_filmes.replace("Top", "Matrix"))
