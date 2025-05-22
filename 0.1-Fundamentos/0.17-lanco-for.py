b31015696e8b94deafc2a417c056671c336fae47
Joao-Inacio <joaoinacio206@protonmail.com>
2025-05-22 15:44:34 -0300

    For

---
+++ 0.1-Fundamentos/0.17-lanco-for.py
@@ -0,0 +1,13 @@
+
+listaFruta = ["Maça", "Banana", "Limão", "Abacaxi"]
+
+for fruta in listaFruta:
+    if fruta == "Limão":
+        break
+    print(fruta)
+
+for fruta in listaFruta:
+    if fruta == "Limão":
+        continue
+    print(fruta)
+
